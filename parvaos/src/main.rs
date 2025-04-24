bae9d4b72e773140649ba51a305f6b624b743232
Francesco Giannice <giannice.developer@gmail.com>
2025-04-24 23:47:32 +0200

    Created a way to make a Rust executable that does not link the Rust std

--- parvaos/src/main.rs
+++ parvaos/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // Disables the Rust standard library for freestanding environments.
+#![no_main] // Disables the default `main` function to define a custom entry point.
+
+use core::panic::PanicInfo; // Imports the `PanicInfo` type for handling panics.
+
+#[no_mangle] // Prevents the compiler from mangling the name of the `_start` function.
+pub extern "C" fn _start() -> ! { // Defines the entry point `_start` with C calling convention and no return.
+    loop {} // Enters an infinite loop since there's no OS to terminate the program.
+}
+
+// This function is called in case of panic.
+#[panic_handler] // Marks the function as the panic handler for the program.
+fn panic(_info: &PanicInfo) -> ! { // Defines the panic handler that takes a `PanicInfo` reference and never returns.
+    loop {} // Halts the program in an infinite loop upon a panic.
 }
