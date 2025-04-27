321229f9bdcf5aab3befd1e4e07311a776877ea2
Francesco Giannice <giannice.developer@gmail.com>
2025-04-27 17:51:51 +0200

    Automatically closed QEMU window without any error during tests

--- parva_os/src/main.rs
+++ parva_os/src/main.rs
@@ -4,7 +4,7 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo; // Imports the `PanicInfo` type for handling panics.
+use core::{arch::asm, panic::PanicInfo}; // Imports the `PanicInfo` type for handling panics.
 
 mod vga;
 
@@ -33,6 +33,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
         test();
     }
     println!("All tests passed!");
+    exit_qemu(QemuExitCode::QEMU_EXIT_SUCCESS);
 }
 
 #[test_case]
@@ -40,4 +41,19 @@ fn trivial_assertion() {
     print!("trivial_assertion...");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    QEMU_EXIT_SUCCESS = 0x10,
+    QEMU_EXIT_FAILURE = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    unsafe {
+        // The `out` instruction is used to send data to a port in x86 assembly.
+        // The `0xf4` port is used by QEMU to exit the emulator.
+        asm!("out dx, eax", in("dx") 0xf4, in("eax") exit_code as u32);
+    }
 }
