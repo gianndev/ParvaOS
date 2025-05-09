db67e9cf4c55a015c194e8fec33fdee86aa16ceb
Francesco Giannice <giannice.developer@gmail.com>
2025-05-09 21:08:35 +0200

    Create the basis for defining each block in the file system

---
+++ parva_os/src/parva_fs/block.rs
@@ -0,0 +1,19 @@
+pub struct Block {
+    // TODO
+}
+
+impl Block {
+    pub fn new() -> Self {
+        Self {
+            // TODO
+        }
+    }
+
+    pub fn read(&self) {
+        // TODO
+    }
+
+    pub fn write(&self) {
+        // TODO
+    }
+}
--- parva_os/src/parva_fs/mod.rs
+++ parva_os/src/parva_fs/mod.rs
@@ -1,4 +1,5 @@
 pub mod super_block;
 pub mod block_device;
+pub mod block;
 
 pub const VERSION: u8 = 1;
